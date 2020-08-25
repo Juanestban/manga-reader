615b39ccc76207752d2cbc674fa94c8e1fbd1c45
juanestban <53704567+Juanestban@users.noreply.github.com>
2020-08-25 14:42:11 -0500

    Updated ModalSessionPortal
    Terminando el modal de inicio de sesion -(Faltante el modal para el registro de aplicacion)

--- components/Modal/Session/modalSesion.js
+++ components/Modal/Session/modalSesion.js
@@ -2,12 +2,211 @@ import ModalPortal from "../modal";
 import ButtonSesion from "../../Buttons/buttonSesion";
 import { useState } from "react";
 
-export default function ModalSession({ changeClick, stateTypeSession = true }) {
-    const [sessionState, setSessionState] = useState(stateTypeSession);
-    // Componente Login y Register para haer una ternaria que me muestre determinado compoenente
-    // dependiendo de la prop que este cambiando de estado
+import Google from '../../Icon/Google';
 
-    const StateSesion = ({ title, nameButton }) => {
+
+export default function ModalSession({ changeClick, stateTypeSession = true, handleUSerGoogle }) {
+    const [sessionState, setSessionState] = useState(stateTypeSession);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('the Button has Clicked');
+    }
+
+    const FormLogin = ({ nameButton }) => {
+        return (
+            <>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                    </div>
+                    <div className="form-input">
+                        <input
+                            id="username"
+                            className="inputsText"
+                            type="text"
+                            name="username"
+                            className="from-control" />
+                    </div>
+                    <div className="form-group password-form">
+                        <label htmlFor="password">Password</label>
+                        <a className="ForgotPassword">Olvidaste tu contraseña?</a>
+                    </div>
+                    <div className="form-input">
+                        <input
+                            id="password"
+                            className="inputsText"
+                            type="password"
+                            name="passsword"
+                            className="from-control" />
+                    </div>
+                    <div className="checkbox-control">
+                        <input
+                            id="remember"
+                            type="checkbox"
+                            name="remember" />
+                        <label htmlFor="remember">Remember</label>
+                    </div>
+                    <li>
+                        <button
+                            className="btnToLogin"
+                            onClick={handleSubmit}>
+                            {nameButton}
+                        </button>
+                    </li>
+                </form>
+
+                <style jsx>{`
+                    .form-group {
+                        display: flex;
+                        margin-bottom: 8px;
+                    }
+
+                    .password-form {
+                        justify-content: space-between;
+                    }
+
+                    label {
+                        font-size: 13px;
+                    }
+
+                    .ForgotPassword {
+                        font-size: 13px;
+                        color: #6777ef;
+                        cursor: pointer;
+                    }
+
+                    .ForgotPassword:hover {
+                        text-decoration: underline;
+                    }
+
+                    .form-input {
+                        display: flex;
+                        margin-bottom: 20px;
+                    }
+
+                    input[type="text"], input[type="password"] {
+                        width: 100%;
+                        padding: 0px 15px;
+                        border-radius: 0.25rem;
+                        font-size: 14px;
+                        height: 42px;
+                        border: 1px solid #ced4da;
+                        text-align: left;
+                    }
+
+                    input[type="text"]:hover, 
+                    input[type="password"]:hover {
+                        color: #212529;
+                    }
+
+                    input[type="text"]:active, 
+                    input[type="text"]:focus,
+                    input[type="password"]:active,
+                    input[type="password"]:focus {
+                        border-color: #6777ef;
+                    }
+
+                    .checkbox-control label,
+                    .checkbox-control input  {
+                        cursor: pointer;
+                    }
+
+                    li {
+                        list-style: none;
+                    }
+
+                    .btnToLogin {
+                        display: inline-block;
+                        width: 100%;
+                        text-align: center;
+                        line-height: inherit;
+                        height: 40px;
+                        margin-top: 20px;
+                        background-color: #6777ef;
+                        color: #fff;
+                    }
+
+                    .btnToLogin:hover {
+                        background-color: #4f5ece;
+                        color: #fff;
+                    }
+                `}</style>
+            </>
+        );
+    }
+
+    const Login = ({ title, nameButton }) => {
+        return (
+            <>
+                <div className="title">
+                    <h4>{title}</h4>
+                </div>
+                <div className="body">
+                    <FormLogin nameButton={nameButton} />
+                    <ButtonSesion
+                        className="btnToLogGoogle"
+                        modalState={handleUSerGoogle}>
+                        <Google width={24} height={24} />
+                        <div style={{ height: 24 }}>Login With Google</div>
+                    </ButtonSesion>
+                    <div style={{
+                        height: 0,
+                        margin: '1.5rem 0 .2rem 0',
+                        overflow: 'hidden',
+                        borderTop: '1px solid #dfe2e5'
+                    }}>
+                    </div>
+                    <ButtonSesion
+                        className="btnToRegister"
+                        modalState={() => setSessionState(false)}>
+                        didn't you has register? Sign up here
+                    </ButtonSesion>
+                </div>
+
+                <style jsx>{`
+                    :global(.btnToLogGoogle) {
+                        display: flex;
+                        justify-content: center;
+                        line-height: inherit;
+                        align-items: center;
+                        width: 100%;
+                        height: 40px;
+                        text-align: center;
+                        margin-top: 15px;
+                        border: 1px solid #f0f0f0;
+                        border-bottom: 2px solid #ccc;
+                        /*border-image-source: linear-gradient(
+                            45deg, 
+                            red, 
+                            orange
+                        );
+                        border-image-slice: 1; */
+                    }
+
+                    :global(.btnToLogGoogle svg) {
+                        margin-right: 5px;
+                    }
+
+                    :global(.btnToLogGoogle:hover) {
+                        color: #000;
+                        background-color: #f2f2f2;
+                    }
+
+                    :global(.btnToRegister) {
+                        font-size: 15px;
+                    }
+
+                    :global(.btnToRegister:hover) {
+                        color: #4f5ece;
+                        text-decoration: underline;
+                    }
+                `}</style>
+            </>
+        );
+    }
+
+    const Register = ({ title, nameButton }) => {
         return (
             <>
                 <div className="seccion-state">
@@ -16,7 +215,7 @@ export default function ModalSession({ changeClick, stateTypeSession = true }) {
                         sign in
                     </ButtonSesion>
                     <ButtonSesion
-                    modalState={() => setSessionState(false)}>
+                        modalState={() => setSessionState(false)}>
                         sign up
                     </ButtonSesion>
                 </div>
@@ -33,7 +232,7 @@ export default function ModalSession({ changeClick, stateTypeSession = true }) {
                 <style jsx>{`
                     .seccion-state {
                         display: flex;
-                        justify-content: space-between;
+                        justify-content: center;
                         align-items: center;
                     }
                 `}</style>
@@ -45,11 +244,11 @@ export default function ModalSession({ changeClick, stateTypeSession = true }) {
         <>
             <ModalPortal onClose={changeClick}>
                 {sessionState
-                    ? <StateSesion
-                        title="Esto es el Login"
+                    ? <Login
+                        title="Login"
                         nameButton="Sign in" />
-                    : <StateSesion
-                        title="Esto es el Registro"
+                    : <Register
+                        title="Register"
                         nameButton="Sign up" />
                 }
             </ModalPortal>
@@ -58,6 +257,13 @@ export default function ModalSession({ changeClick, stateTypeSession = true }) {
                 .body > :global(li button) {
                     border: 1px solid #09f;
                     margin-bottom: 10px;
+                }
+
+                :global(.modal) {}
+
+                :global(.modal .modal-content) {
+                    width: 340px;
+                    box-shadow: 0 0.46875rem 2.1875rem rgba(90,97,105,0.1), 0 0.9375rem 1.40625rem rgba(90,97,105,0.1), 0 0.25rem 0.53125rem rgba(90,97,105,0.12), 0 0.125rem 0.1875rem rgba(90,97,105,0.1);
                 }
             `}</style>
         </>
