4fedbfd718eb4bf45cafcc4c7164eee9eeb818cf
juanestban <53704567+Juanestban@users.noreply.github.com>
2020-08-25 14:40:05 -0500

    Updated Access data User
    datos de acceso al usuario por cambios de estados en el login (Saber si el usuario inicio sesion o cerro sesion - Nunca inicio sesion)

--- firebase/client.js
+++ firebase/client.js
@@ -1,6 +1,23 @@
 import firebase from './key';
 
 
+const mapUserFromFirebaseAuthToUser = (user) => {
+    const { displayName, photoURL } = user;
+    return {
+        firstName: displayName,
+        avatar: photoURL
+    };
+}
+
+export const onAuthStateChanged = (onChange) => {
+    return firebase
+        .auth()
+        .onAuthStateChanged(user => {
+            const normaliedUser = mapUserFromFirebaseAuthToUser(user);
+            onChange(normaliedUser);
+        })
+}
+
 export const loginWithGoogle = () => {
     const googleProvider = new firebase.auth.GoogleAuthProvider();
     return firebase.auth().signInWithPopup(googleProvider);
