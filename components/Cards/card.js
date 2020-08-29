47f569ee0c0f3273437acfe1b95cbb351051a9b8
juanestban <53704567+Juanestban@users.noreply.github.com>
2020-08-29 14:45:58 -0500

    Components Cards
    Usando metodos del firestore par obtener los datos de los mangas que estan en la base de datos de dicho servicio

--- components/Cards/card.js
+++ components/Cards/card.js
@@ -1,50 +1,25 @@
-// Hacer un fetch para traer todos los componentes y datos de las cards
-// en otro componente
-// desestructuracion del objeto que obtenga del firebase
-const thumbImg = {
-    BerserkGluttony: '/img/imgCards/BerserkOfGluttony.jpg',
-    Konosuba: '/img/imgCards/Konosuba.png',
-    Masamune: '/img/imgCards/Masamune-Kun.jpg',
-    Oregairu: '/img/imgCards/Oregairu.jpg',
-    OtomeGame: '/img/imgCards/OtomeGame.png',
-    TateNoYuusha: '/img/imgCards/TateNoYuusha.jpg',
-    OregairuNovels: '/img/imgCards/thumbOregairu.png',
-    TokyoRavens: '/img/imgCards/TokyoRavens.png',
-    Toradora: '/img/imgCards/Toradora.jpg'
-}
+export default function Card({ dataGetter }) {
+    const {id, name, imgPortada, category} = dataGetter;
 
-const imgPortada = Object.keys(thumbImg);
-
-
-// Ajustes de estilos casi finalizados
-// Falta que el contenido conforme el genero que sea el background cambie para mas denamismo al sitio web
-// ¡Hacer del componenete estatico a Dinamico!
-export default function Card() {
     return (
         <>
-            {imgPortada.map((imgPortada) => {
-                return (
-                    <div
-                        className="card"
-                        key={imgPortada}>
-                        <div className="text">
-                            <h4>Berserk of Gluttony</h4>
-                        </div>
-                        <img
-                            src={thumbImg[imgPortada]}
-                            alt="Oregairu" />
-                        <div className="text text-bottom">
-                            <h4>Shonen</h4>
-                        </div>
-                    </div>
-                );
-            })}
+            <div className="card">
+                <div className="text">
+                    <h4>{name}</h4>
+                </div>
+                <img
+                    src={imgPortada}
+                    alt="Oregairu" />
+                <div className={'text text-category ' + category}>
+                    <h4>{category}</h4>
+                </div>
+            </div>
             <style jsx>{`
                 .card {
                     width: 15%;
                     height: 270px;
                     margin: 10px 10px;
-                    over-flow: hidden;
+                    overflow: hidden;
                     box-sizing: border-box;
                     position: sticky;
                 }
@@ -52,10 +27,40 @@ export default function Card() {
                 .text {
                     width: 100%;
                     position: absolute;
-                    text-align: center;
+                    padding-left: 8px;
                     background-color: rgba(60, 65, 66, 0.6);
                     color: #fff;
                     z-index: 1;
+                }
+
+                .text h4 {
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    white-space: nowrap;
+                    font-size: .96rem;
+                }
+
+                .text-category {
+                    text-align: center;
+                    bottom: 15px;
+                }
+
+                /*Categorias de mangas*/
+
+                .Seinen {
+                    background-color: rgba(220, 53, 69, 0.9);
+                }
+
+                .Shonen {
+                    background-color: rgba(253, 126, 20, 0.9);
+                }
+
+                .Shoujo {
+                    background-color: rgba(232, 62, 140, 0.9);
+                }
+
+                .Novel {
+                    background-color: rgba(34, 136, 170, 0.9);
                 }
 
                 img {
@@ -65,11 +70,6 @@ export default function Card() {
 
                 h4 {    
                     margin: 0;
-                }
-
-                .text-bottom {
-                    background-color: rgba(253, 126, 20, 0.9);
-                    bottom: 15px;
                 }
             `}</style>
         </>
--- components/Cards/listCards.js
+++ components/Cards/listCards.js
@@ -1,4 +1,9 @@
+import { useState, useEffect } from "react";
+import { getAllMangas } from '../../firebase/mangas';
+
+// component Card
 import Card from "./card";
+
 
 const styleContentMarco = {
     display: 'flex',
@@ -12,9 +17,21 @@ const styleContentMarco = {
 
 
 export default function ListCards() {
+    const [values, setValues] = useState([]);
+
+    useEffect(() => {
+        getAllMangas(arrayMangas => {
+            setValues(arrayMangas);
+        });
+    }, []);
+
     return (
         <div style={styleContentMarco}>
-            <Card />
+            {values.map(manga => (
+                <>
+                    <Card key={manga.id} dataGetter={manga} />
+                </>
+            ))}
         </div>
     );
 }
