676aee85e31e7a12794a4b35f2b16c2ac376e2d9
juanestban <53704567+Juanestban@users.noreply.github.com>
2020-09-07 12:59:06 -0500

    bug fixing changes to component update Mangas

--- pages/uploadMangas/index.js
+++ pages/uploadMangas/index.js
@@ -3,51 +3,99 @@ import Generos from '../../components/ComponentGeners/generos';
 
 // style Forms
 import { styleForms } from '../../components/AppLayout/styles';
+import { useEffect, useState } from 'react';
 
 
 // Este componente estara disponible solo a las usuarios registrados === undefined || === user.json
 
-const handleUploaderSubmit = (e) => {
-    e.preventDefault();
-    console.log('clicked Button submit Form Uploader Mangas');
-}
-
 const FormUpload = () => {
+    const initialManga = {
+        name: '',
+        description: '',
+        category: '',
+        generos: [],
+        imgCard: '',
+        imgPortada: '',
+        type: 'Manga'
+    };
+
+    const [manga, setManga] = useState(initialManga);
+    const [ stateType, setStateType] = useState(true);
+
+    const checkedValue = (value) => {
+        setManga({...manga, generos: value });
+    }
+
+    const changeInputs = (e) => {
+        const {name, value } = e.target;
+        setManga({...manga, [name]: value});
+    }
+
+    const typeContent = (e) => {
+        e.preventDefault();
+
+        if (!stateType) {
+            setManga({...manga, type: 'Manga'});
+        } else {
+            setManga({...manga, type: 'Novel'});
+        }
+
+        setStateType(!stateType);
+    }
+
+    const handleUploaderSubmit = (e) => {
+        e.preventDefault();
+        console.log(manga);
+    }
+
+    useEffect(() => {
+        checkedValue();
+    }, [])
+
     return (
         <>
             <div className="contentForm">
-                <form onSubmit={handleUploaderSubmit}>
-                    <div className="form-group">
+                <form>
+                    <div className="form-group" style={{justifyContent: 'space-between'}}>
                         <label htmlFor="Form">Nombre del manga</label>
+                        <div className="content-type">
+                            <p>type content:</p>
+                            <button onClick={typeContent}>
+                                {stateType 
+                                ? 'Manga' 
+                                : 'Novel'}
+                            </button>
+                        </div>
                     </div>
                     <div className="form-input">
                         <input
+                            onChange={changeInputs}
+                            name="name"
                             type="text" />
                     </div>
                     <div className="form-group">
                         <label htmlFor="Form">Descripcion del manga</label>
                     </div>
                     <div className="form-input">
-                        <textarea name="description" rows="12"></textarea>
+                        <textarea 
+                            onChange={changeInputs} 
+                            name="description" 
+                            rows="12">
+                        </textarea>
                     </div>
                     <div className="form-group">
                         <label htmlFor="Form">Genero</label>
                     </div>
-                    <Generos />
-                    <div className="form-input">
-                        <input
-                            type="text" />
-                    </div>
+                    <Generos valueSent={checkedValue} />
                     <div className="form-group">
                         <label htmlFor="Form">Categoria</label>
                     </div>
                     <div className="form-input">
-                        <select name="" id="">
+                        <select onChange={changeInputs} name="category">
                             <option disabled>Selecciona una Categoria</option>
                             <option value="Seinen">Seinen</option>
                             <option value="Shonen">Shonen</option>
                             <option value="Shoujo">Shoujo</option>
-                            <option value="Novel">Novel</option>
                         </select>
                     </div>
                     <div className="form-group">
@@ -72,7 +120,9 @@ const FormUpload = () => {
                             className="buttonFileUpload"
                             type="file" />
                     </div>
-                    <button className="buttonSubmitFormUploader">Subir</button>
+                    <button
+                        onClick={handleUploaderSubmit}
+                        className="buttonSubmitFormUploader">Subir</button>
                 </form>
             </div>
 
